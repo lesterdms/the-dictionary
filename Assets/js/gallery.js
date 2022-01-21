d89c6fb748cf8b3ae0885a1a9e8239f5e445377d
lesterdms <69951564+lesterdms@users.noreply.github.com>
2022-01-21 17:41:28 +0800

    Delete Assets directory

--- Assets/js/gallery.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing-3",{
-    strings: ["Designer", "UI/UX Designer", "Game Developer", "Web Developer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
