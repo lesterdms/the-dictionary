abaee5f1ee107e831653e5f3437e992a7bd60c82
lesterdms <69951564+lesterdms@users.noreply.github.com>
2022-01-21 12:14:33 +0800

    Add files via upload

---
+++ Assets/js/gallery.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing-3",{
+    strings: ["Designer", "UI/UX Designer", "Game Developer", "Web Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
